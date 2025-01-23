6b5e589f48243d79473eefd25ecf15e6013746dd
pricelenoir <price.lenoir@gmail.com>
2025-01-23 16:04:10 -0500

    Update

--- glm-app/App.js
+++ glm-app/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useBLE } from './services/useBLE';
-import BLEScreen from './screens/BLEScreen';
+// import { useBLE } from './services/useBLE';
+// import BLEScreen from './screens/BLEScreen';
 import MainScreen from './screens/MainScreen';
 
 function AppNavigator() {
-  const { isConnected } = useBLE();
+  // const { isConnected } = useBLE();
+  const isConnected = true; // Temporarily hardcode to true
   return isConnected ? <MainScreen /> : <BLEScreen />;
 }
 
